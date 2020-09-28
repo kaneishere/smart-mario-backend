ba0c555cecc6d3a4072555f66e3fecf3488171e7
AdiVH <adityavh1@gmail.com>
2020-09-28 21:31:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 app.use(bodyParser.json()) 
+app.use(bodyParser.urlencoded({extended:true})) 
+
 
 // Set Header Content-Type = application/json (no quotes)
 
