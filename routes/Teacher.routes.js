32b70aaec5419a90be8054e927e3b778cf59ee67
kaneishere <bwu009@e.ntu.edu.sg>
2020-09-28 23:28:21 +0800

    format

--- routes/Teacher.routes.js
+++ routes/Teacher.routes.js
@@ -1,11 +1,11 @@
-module.exports = app => {
-    const teacher_fns = require("../controllers/Teacher.controllers.js")
+module.exports = (app) => {
+  const teacher_fns = require("../controllers/Teacher.controllers.js");
 
-    var router = require("express").Router()
+  const router = require("express").Router();
 
-    router.get("/", teacher_fns.findAll)
-    router.post("/", teacher_fns.createTeacher)
-    router.post("/authenticate", teacher_fns.authenticate)
+  router.get("/", teacher_fns.findAll);
+  router.post("/", teacher_fns.createTeacher);
+  router.post("/authenticate", teacher_fns.authenticate);
 
-    app.use("/api/teachers", router)
-}
+  app.use("/api/teachers", router);
+};
