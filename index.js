b797e6d4ba3cdeb6aefe28105376bba851b4b5b8
AdiVH <adityavh1@gmail.com>
2020-09-29 12:03:08 +0800

    migrated to sqllite

--- index.js
+++ index.js
@@ -9,18 +9,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Set Header Content-Type = application/json (no quotes)
 
 const db = require("./models/index.js");
+
 db.sequelize.sync({ force: false }).then(() => {
   console.log("DB Synced");
 });
 
 // Test call
+
 let count = 1;
 app.get("/", function (req, res) {
   res.send("API Call: " + count++);
 });
 
-require("./routes/Student.routes")(app)
-require("./routes/Teacher.routes")(app)
+require("./routes/Student.routes")(app);
+require("./routes/Teacher.routes")(app);
 require("./routes/Question.routes")(app);
 
 const PORT = process.env.PORT || 3000;
